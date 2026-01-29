f25ae9bc81708b40a0191e7a4b81da2e2e7b8f2b
soe-student <soe.kaythi38@student.qcc.cuny.edu>
2026-01-29 12:48:41 -0500

    update testing

--- lab2/lab2_kaythi.py
+++ lab2/lab2_kaythi.py
@@ -1,5 +1,11 @@
 """
 Soe Kaythi
-Lab-2
+Lab-2: Git Commands
 """
-print("\nSoe Kaythi, Welcome to Git Commands!")
+print("\nWelcome to Git Commands!")
+username = input("Enter your username: ")
+print(f"Hello, {username}! Let's explore some Git commands together.")
+
+# Testing Lines
+num = 5
+print(f"Double of number {num}: {num * 2}")
