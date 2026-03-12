d36dae20256828a4aebb131f836f5a631c236140
soe-student <soe.kaythi38@student.qcc.cuny.edu>
2026-03-11 23:57:11 -0400

    lab11

---
+++ lab11/app.py
@@ -0,0 +1,27 @@
+
+"""
+Lab 11, Introduction to Flask
+March 10, 2026
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    name = "Prof.Wu"
+    fruits = ["Apple", "Banana", "Cherry"]
+    fruit = 'pineapple'
+    return render_template('index.html', username=name, fruitslist=fruits, f=fruit)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/quotes')
+def quotes():
+    return '<h1>Quotes</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
